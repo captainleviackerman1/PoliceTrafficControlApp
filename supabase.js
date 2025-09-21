f56f5318a83f1833299f6fd22e53afe576715065
kavish yadav <akaug2023@gmail.com>
2025-09-21 12:02:59 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Use the URL and key your friend gave you
 const supabaseUrl = 'https://ylkmgjpodraeyvtbeouf.supabase.co'
-const supabaseKey = 'YOUR_ANON_KEY_HERE'
+const supabaseKey = 'eyJhbGciOiJIUzl1NilslnR5cCI6IKpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSlslnJIZil6lnlsa21nanBvZHJhZXL2dGJIb3Vmliwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0mjY5ODgslmV4cCI6MjA3NDAwMjk4Oh0.MfC--yVM00uLbv9Oz8e6AJa669fVhatrjeFZTKx4CKg'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
